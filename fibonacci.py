be655b3f2b17867e6a82d7936901189a3bc22490
WAMBURA Carolyne Aniceth <140801935+carolynewambura06@users.noreply.github.com>
2024-03-19 01:49:19 -0700

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -3,7 +3,7 @@ def fibonaci(n):
     for i in range (2, n):
         next_term = sequence[i-1] + sequence[i-2]
         sequence.append(next_term)
-        return sequence[:n] #[:n] start the beginning of the list and include all the elements except of index n
+    return sequence[:n] #[:n] start the beginning of the list and include all the elements except of index n
     
 nums =int(input('Enter the value of n: '))
 fibonacci_sequence = fibonaci(nums)
